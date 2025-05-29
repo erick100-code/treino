5fb84bac4c37f6b6a56a0825f7da81d98c16eadd
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-05-29 18:47:17 -0300

    cronometro finalizado

---
+++ javascript/cronometro2.js
@@ -0,0 +1,68 @@
+const inici = document.getElementById('play')
+const action = inici.getAttribute('action')
+const localmarc = document.getElementById('marc')
+const titulo = document.getElementById('tempo')
+const botMarc = document.getElementById('marcar')
+const botreset = document.getElementById('reset')
+
+let timer = 0
+let intervalId = 0
+let marcs = []
+
+const formatTime = (time) => { // FORMATA O TEMPO
+    const horas = Math.floor(time / 360000)
+    const minutos = Math.floor((time % 360000) / 6000)
+    const segundos = Math.floor((time % 6000) / 100)
+    const microS = time % 100
+
+    return `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}:${microS.toString().padStart(2, '0')}`
+}// RETORNA O FORMATO FORMATADO
+
+const marcar = (casaN, tempoT) => {
+    localmarc.innerHTML += `<p>marcação ${casaN}: ${formatTime(tempoT)}<p/>`
+}
+
+const agoraMarc = () => {
+    if (timer > 0) {
+        marcs.push(timer)
+        marcar(marcs.length, timer)
+    }
+}
+
+const iniciaTimer = () => {
+    const bot = document.getElementById('play')
+    const act = bot.getAttribute('action')
+
+    clearInterval(intervalId)
+
+    if (act == 'start' || act == 'continue') {
+        intervalId = setInterval( () => {
+            timer += 1
+            mostraTimer(timer)
+        }, 10)
+
+        bot.innerHTML = '<i class="fa-solid fa-pause"></i>'
+        bot.setAttribute('action', 'pause')
+    } else if (act == 'pause') {
+        bot.setAttribute('action', 'continue')
+        bot.innerHTML = '<i class="fa-solid fa-play"></i>'
+    }
+}
+
+const mostraTimer = (time) => {
+    titulo.innerHTML = `${formatTime(time)}`
+}
+
+const reset = () => {
+    clearInterval(intervalId)
+    timer = 0
+    mostraTimer(timer)
+    inici.innerHTML = `<i class="fa-solid fa-play"></i>`
+    inici.setAttribute('action', 'start')
+    localmarc.innerHTML = ''
+    marcs = []
+}
+
+inici.addEventListener("click", iniciaTimer)
+botMarc.addEventListener("click", agoraMarc)
+botreset.addEventListener("click", reset)
---
+++ objeto.js
@@ -0,0 +1,9 @@
+let arrayobjt = [
+    {nome: 'erick', idade: 30, profissão: 'caçador de demonios'},
+    {nome: 'daniel', idade: 27, profissão: 'policial militar'},
+]
+
+console.log(arrayobjt[0])
+// let obj = {numero: 1, idade: 18, nome: 'erick'}
+
+// console.log(obj.idade)
