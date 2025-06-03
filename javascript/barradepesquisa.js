230e372b8c5e3ecc40cae7c60a2fb2cb499cd1e4
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-06-03 18:14:18 -0300

    barra de pesquisa

---
+++ javascript/barradepesquisa.js
@@ -0,0 +1,36 @@
+const input = document.getElementById('barra')
+
+input.addEventListener('input', (event) => {
+    const valorinp = formatar(event.target.value)
+    
+    const items = document.querySelectorAll('.item')
+    const  nenhum = document.getElementById('nen')
+    const ul = document.getElementById('caxa')
+    let temRes = false
+
+    items.forEach(item => {
+        const title = item.querySelector('.title')
+
+        let encontrado = false
+
+        if (formatar(title.textContent).indexOf(valorinp) != -1) {
+            item.style.display = 'flex'
+            temRes = true
+            encontrado = true
+        } else {
+            item.style.display = 'none'
+        }
+    })
+
+    if (temRes) {
+        ul.style.height = '400px'
+        nenhum.style.display = 'none'
+    } else {
+        ul.style.height = 'auto'
+        nenhum.style.display = 'block'
+    }
+})
+
+function formatar(value) {
+    return value.toLowerCase().trim()
+}
