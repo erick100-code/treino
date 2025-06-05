59ade912f695763cb6eae6e29ef92affcbacbf14
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-06-05 16:26:05 -0300

    modal feito

---
+++ javascript/objeto.js
@@ -0,0 +1,7 @@
+const obj = [//ARRAY DE OBJETOS
+    {nome: 'erick', idade: 17, senha: 123452, saldoBancario: 124.000},
+    {nome: 'eduardo', idade: 17, senha: 5555, saldoBancario: 34.000},
+    {nome: 'araujo', idade: 17, senha: 776767, saldoBancario: 3.000},
+]
+
+console.log(obj)
